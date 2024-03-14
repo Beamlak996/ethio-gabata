0890922eea96f9d181308dad187da5b0951ef049
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-03-14 09:11:02 -0900

    Logo Changed

--- components/logo.tsx
+++ components/logo.tsx
@@ -10,10 +10,8 @@ export const Logo = () => {
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 flex">
         {/* <Image src="/logo.svg" alt="logo" height={30} width={30} /> */}
-        <p
-          className={cn("text-lg text-neutral-700 pb-1 font-bold")}
-        >
-          Ethio <span className="text-green-500" >Gabata</span>
+        <p className={cn("text-lg text-neutral-700 pb-1 font-bold")}>
+          Ethio <span className="text-green-500">ገበታ</span>
         </p>
       </div>
     </Link>
