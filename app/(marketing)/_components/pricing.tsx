1cc7f523f4654282551da1d3d4f85e168ba7476e
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-03-19 11:31:04 -0900

    Pricing Done

--- app/(marketing)/_components/pricing.tsx
+++ app/(marketing)/_components/pricing.tsx
@@ -8,21 +8,21 @@ export function Pricing() {
         Flexible plans for you
       </h1>
       <p className="pt-[16px] pb-[40px] text-center text-[#36485C] lg:text-[18px]">
-        No hidden fees!
+        First Package
       </p>
 
       <div className="flex flex-col gap-y-6 gap-x-[24px] lg:flex-row">
         <div className="w-full rounded-[8px] bg-[#F5F4FF] p-6 flex flex-col lg:justify-between">
           <div>
             <h3 className="font-medium text-[#4328EB] text-[18px] lg:text-xl">
-              Free Trial
+              Basic
             </h3>
             <p className="pt-[12px] text-[#36485C] lg:text-[18px]">
               Perfect for testing the waters
             </p>
 
             <h2 className="pt-4 text-2xl font-medium lg:text-[32px]">
-              0$<span className="text-[#5F7896]">/mo</span>
+              2000 Birr
             </h2>
 
             <ul className="flex flex-col gap-y-2 pt-4 text-[#5F7896]">
@@ -30,19 +30,21 @@ export function Pricing() {
                 <span>
                   <Image src={Check} alt="included" />
                 </span>
-                Lorem ipsum dolor sit amet
+                For freedom of time
               </li>
               <li className="flex items-center gap-x-2">
                 <span>
                   <Image src={Check} alt="included" />
                 </span>
-                Lorem ipsum dolor sit amet
+                Phone Number:{" "}
+                <span className="text-muted-foreground">0919042420</span>
               </li>
               <li className="flex items-center gap-x-2">
                 <span>
                   <Image src={Check} alt="included" />
                 </span>
-                Lorem ipsum dolor sit amet
+                Bank Account:{" "}
+                <span className="text-muted-foreground">1000***90765</span>
               </li>
             </ul>
           </div>
@@ -54,14 +56,14 @@ export function Pricing() {
         <div className="w-full rounded-[8px] bg-[#4328EB] p-6 flex flex-col">
           <div>
             <h3 className="font-medium text-white text-[18px] lg:text-xl">
-              Business
+              Golden package
             </h3>
             <p className="pt-[12px] text-[#F4F8FA] lg:text-[18px]">
-              Perfect for small businesses
+              The best package.
             </p>
 
             <h2 className="pt-4 text-2xl font-medium text-white lg:text-[32px]">
-              500$<span className="text-[#F4F8FA]">/mo</span>
+              11,000
             </h2>
 
             <ul className="flex flex-col gap-y-2 pt-4 text-[#F4F8FA]">
@@ -69,31 +71,19 @@ export function Pricing() {
                 <span>
                   <Image src={Check} alt="included" />
                 </span>
-                Lorem ipsum dolor sit amet
+                For better tomorrow
               </li>
               <li className="flex items-center gap-x-2">
                 <span>
                   <Image src={Check} alt="included" />
                 </span>
-                Lorem ipsum dolor sit amet
+                Phone Number: <span className="">0919042420</span>
               </li>
               <li className="flex items-center gap-x-2">
                 <span>
                   <Image src={Check} alt="included" />
                 </span>
-                Lorem ipsum dolor sit amet
-              </li>
-              <li className="flex items-center gap-x-2">
-                <span>
-                  <Image src={Check} alt="included" />
-                </span>
-                Lorem ipsum dolor sit amet
-              </li>
-              <li className="flex items-center gap-x-2">
-                <span>
-                  <Image src={Check} alt="included" />
-                </span>
-                Lorem ipsum dolor sit amet
+                Bank Account: <span className="">1000***90765</span>
               </li>
             </ul>
           </div>
@@ -105,22 +95,16 @@ export function Pricing() {
         <div className="w-full rounded-[8px] bg-[#F5F4FF] p-6 flex flex-col lg:justify-between">
           <div>
             <h3 className="font-medium text-[#4328EB] text-[18px] lg:text-xl">
-              Enterprise
+              Diamond level
             </h3>
             <p className="pt-[12px] text-[#36485C] lg:text-[18px]">
-              Perfect for big companies
+              Perfect for big buissness
             </p>
 
-            <h2 className="pt-4 text-2xl font-medium lg:text-[32px]">Custom</h2>
+            <h2 className="pt-4 text-2xl font-medium lg:text-[32px]">6000 Birr</h2>
 
             <p className="pt-4 text-[16px] text-[#36485C]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore.
-            </p>
-            <p className="pt-2 text-[16px] text-[#36485C]">
-              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-              aut fugit. Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit.
+              Freedom of money
             </p>
           </div>
           <button className="mt-[16px] rounded-[4px] bg-white py-[14px] text-[#4328EB] font-medium">
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "./_components/hero";
 import Navbar from "./_components/navbar";
+import { Pricing } from "./_components/pricing";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className="relative overflow-hidden" >
+      <div className="relative overflow-hidden p-4" >
         <Hero />
+        <Pricing />
       </div>
     </div>
   );
